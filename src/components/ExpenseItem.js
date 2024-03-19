9a714df18f048aef2a0de0d0cde0f986e86cc7ee
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-03-19 11:24:01 +0530

    calenderformat

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,28 @@
+import "./ExpenseItem.css";
+
+const months = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+function ExpenseItem(props) {
+  const month = months[props.date.getMonth()];
+  const year = props.date.getFullYear();
+  const date = props.date.getDate();
+
+  return (
+    <div className="expense-item">
+      <div className="expense-item__date">
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{date}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
